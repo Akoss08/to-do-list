958e1fe227723d91a10924254b6b197210acd0cb
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-02-28 15:10:12 +0100

    implement, CRUD operations for to dos

--- backend/server.py
+++ backend/server.py
@@ -44,9 +44,103 @@ def get_all_todos():
                 "creation_time": todo.creation_time,
                 "is_done": todo.is_done,
             }
+            for todo in all_todos
+        ]
+
+        return jsonify(todos_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.post("/api/todos")
+def add_to_do():
+    try:
+        new_todo = request.get_json()
+
+        if not new_todo["to_do"].strip():
+            raise KeyError
+
+        to_do = Todo(to_do=new_todo["to_do"])
+
+        db.session.add(to_do)
+        db.session.commit()
+
+        return jsonify({"new_to_do_id": to_do.id}), 201
+    except KeyError:
+        return jsonify({"error": "'to_do' key and its value is required"}), 400
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
+@app.patch("/api/todos/<int:id>")
+def update_to_do(id):
+    try:
+        update = request.get_json()
+
+        if not update:
+            raise KeyError("request body required")
+
+        if "to_do" in update.keys() and not update["to_do"].strip():
+            raise KeyError("to_do cannot be empty")
+
+        todo_to_update = Todo.query.get_or_404(
+            id, description=f"to-do with id {id} not found"
         )
 
-    return jsonify(todos_list)
+        immutable_fields = {"id", "creation_time"}
+
+        for key, value in update.items():
+            if key not in immutable_fields:
+                setattr(todo_to_update, key, value)
+
+        db.session.commit()
+
+        return jsonify({"updated_todo_id": todo_to_update.id})
+    except StatementError as e:
+        db.session.rollback()
+        return jsonify({"error": f"Invalid request value: {str(e)}"}), 400
+    except KeyError as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
+@app.delete("/api/todos/<int:id>")
+def delete_to_do(id):
+    try:
+        todo_to_delete = Todo.query.get_or_404(
+            id, description=f"to-do with id {id} not found"
+        )
+
+        db.session.delete(todo_to_delete)
+        db.session.commit()
+
+        return jsonify({"successfully_deleted_id": todo_to_delete.id})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
+@app.get("/api/todos/<int:id>")
+def get_to_do_by_id(id):
+    try:
+        to_do = Todo.query.get_or_404(
+            id, description=f"to-do with id {id} not found"
+        )
+
+        return jsonify(
+            {
+                "id": to_do.id,
+                "to_do": to_do.to_do,
+                "creation_time": to_do.creation_time,
+                "is_done": to_do.is_done,
+            }
+        )
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
