b4137c771a5038c841d4fd924b11cb6cf3f6985b
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-02-28 09:44:56 +0100

    create Todo class

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
 
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = "to_do_list"
+
+    id = db.Column(db.Integer, primary_key=True)
+    to_do = db.Column(db.Text, nullable=False)
+    creation_time = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.now,
+    )
+    is_done = db.Column(db.Boolean, default=False)
 
 
 @app.route("/api/test")
