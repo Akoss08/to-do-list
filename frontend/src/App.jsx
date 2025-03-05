2802e39600be351c09590b24717b1bd12e9126ea
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-05 10:46:47 +0100

    inject Todo component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,41 @@
 import { useEffect, useState } from 'react';
 import api from './api/todos';
+import Todo from '../components/Todo';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    async function getTodos() {
-      try {
-        const response = await api.get();
-        setTodos(response.data);
-      } catch (error) {
-        if (error.response) {
-          console.log(error.response.data.message);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else {
-          console.log(`Error: ${error.message}`);
-        }
+  async function getTodos() {
+    try {
+      const response = await api.get();
+      setTodos(response.data);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else {
+        console.log(`Error: ${error.message}`);
       }
     }
+  }
 
+  useEffect(() => {
     getTodos();
   }, []);
 
   return (
     <>
       {todos && (
-        <div className="h-screen w-screen flex justify-center items-center">
-          <div className="flex items-center flex-col gap-5">
+        <div className="sm:h-screen w-screen flex justify-center items-center">
+          <div
+            className="
+            flex grow sm:grow-0 items-center flex-col rounded-xl w-1/3 max-h-3/4 overflow-y-auto 
+            no-scrollbar bg-violet-400 shadow-[0_0px_50px_rgba(28,25,23,0.8)] scroll-gradient
+          "
+          >
             {todos.map((todo) => (
-              <div>
-                <h1 className="text-blue-300">{todo.id}</h1>
-                <h1 className="text-pink-300">Something</h1>
-              </div>
+              <Todo key={todo.id} task={todo.to_do} date={todo.creation_time} is_done={todo.is_done} id={todo.id} refresh={getTodos} />
             ))}
           </div>
         </div>
